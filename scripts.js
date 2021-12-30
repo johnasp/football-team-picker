d56b1a02f52a0bd99a8a9942fc7af37051b8906e
John Aspinall <john.aspinall73@gmail.com>
2021-12-30 16:37:32 +0000

    JSON data successfully requested

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+// LIST PLAYERS JSON DOWN THE SIDEBAR
+
+// 1. Get reference to players ELement
 let playerEl = document.querySelector('.players') 
 
-console.log(playerEl)  //This is returning null in the console
+// 2. Create request object
+const request = new XMLHttpRequest()
+
+// 3. Check for when the data file has been successfully delivered i.e. 'the ready stats is code 4' 
+request.addEventListener('readystatechange', function() {
+   console.log(request, request.readyState)
+   if (request.readyState === 4) {
+      console.log(request.responseText)
+   }
+})
+
+// 3. Use open method of request object, (passing in GET and file name as parameters) and then send the request
+request.open('GET', 'players.json')
+request.send()
