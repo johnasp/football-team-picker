9650698b12bff079daf3444c63cf7108dd9f7ac6
John Aspinall <john.aspinall73@gmail.com>
2022-01-02 22:34:31 +0000

    442 css grid done

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
 // https://www.youtube.com/watch?v=C3dfjyft_m4 
 
 function renderPlayers() {
-   const playersEl = document.querySelector('.players')
+   const playersEl = document.querySelector('.players-list')
    fetch('players.json')
-      .then(
-         response => response.json()) // converts JSON from string to array
+      .then(response => response.json()) // converts JSON from string to array
       .then(data => {
          for (let i = 0; i < data.players.length; i++ ) {
+            console.log (data)
             const playerName = data.players[i].name
             const shirtNumber = data.players[i].shirtNumber
             const player = shirtNumber + " - " + playerName
-            playersEl.innerHTML += `<p>${player}</p>`
+            playersEl.innerHTML += `<li>${player}</li>`
          }
       })    
 }
