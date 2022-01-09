f7506787251ae8c921bfeeddd7d22cddf65a82fc
John Aspinall <john.aspinall73@gmail.com>
2022-01-09 22:21:47 +0000

    Tidide up select fields and added Google fonts

--- scripts.js
+++ scripts.js
@@ -1,18 +1,22 @@
 // https://www.youtube.com/watch?v=C3dfjyft_m4 
 
 function renderPlayers() {
-   const playersEl = document.querySelector('.players-list')
+   const playersEl = document.querySelector('.controls__players')
    fetch('players.json')
       .then(response => response.json()) // converts JSON from string to array
       .then(data => {
          for (let i = 0; i < data.players.length; i++ ) {
-            console.log (data)
+            //console.log (data)
             const playerName = data.players[i].name
             const shirtNumber = data.players[i].shirtNumber
             const player = shirtNumber + " - " + playerName
-            playersEl.innerHTML += `<li>${player}</li>`
+            playersEl.innerHTML += `<a class='controls__player'>${player}</a>`
          }
       })    
 }
-
 renderPlayers()
+
+let playerItemEl = document.querySelector('.controls__player')
+playerItemEl.addEventListener('click', function() {
+   console.log('test')
+})
