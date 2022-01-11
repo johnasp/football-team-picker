57129e2b5b247fbfd83f191985492edcddb81323
John Aspinall <john.aspinall73@gmail.com>
2022-01-11 23:40:55 +0000

    Drag and drop event handling added

--- scripts.js
+++ scripts.js
@@ -1,5 +1,9 @@
 // https://www.youtube.com/watch?v=C3dfjyft_m4 
 
+
+// ---------------------
+// Bring in the players JSON
+// ---------------------
 function renderPlayers() {
    const playersEl = document.querySelector('.controls__players')
    fetch('players.json')
@@ -10,17 +14,38 @@ function renderPlayers() {
             const playerName = data.players[i].name
             const shirtNumber = data.players[i].shirtNumber
             const player = shirtNumber + " - " + playerName
-            playersEl.innerHTML += `<a class='controls__player'>${player}</a>`
+            playersEl.innerHTML += `<a class="controls__player" id="shirt-${shirtNumber}" draggable="true" ondragstart="dragPlayer(event)">${player}</a>`
          }
       })    
 }
 renderPlayers()
+// ---------------------
+// Drag and drop player
+// ---------------------
+
+//overrides default behviour of the dragEvent which does not allow data/elements to be dragged onto another element
+function allowPlayerDrop(ev) { 
+   ev.preventDefault();
+ }
+ 
+ // Sets the data to transfer on drag
+function dragPlayer(ev) {
+   console.log(ev)
+   ev.dataTransfer.setData("text", ev.target.id)
+}
+
+function dropPlayer(ev) {
+   ev.preventDefault();
+   var data = ev.dataTransfer.getData("text");
+   ev.target.appendChild(document.getElementById(data));
+ }
+
 
 // Create node list of players
 const playerItems = document.querySelectorAll('.controls__players')
 
 playerItems.forEach(item => {
-   item.addEventListener('click', () => {
-      console.log('clicked')
+   item.addEventListener('click', (e) => {
+      console.log(e.path[0].textContent)
    })
 })
