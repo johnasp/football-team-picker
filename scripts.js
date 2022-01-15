ca5ced9d0a48f0a227b42cf8b2498f559838b0dc
John Aspinall <john.aspinall73@gmail.com>
2022-01-15 00:23:46 +0000

    Got player name drag working

--- scripts.js
+++ scripts.js
@@ -20,35 +20,41 @@ renderPlayers()
 // ----------------------------
 // Create node list of players
 // ----------------------------
-const playerItems = document.querySelectorAll('.controls__players')
-
-playerItems.forEach(item => {
-   item.addEventListener('click', (e) => {
-      console.log(e.path[0].textContent)
-   })
-})
+//const playerItems = document.querySelectorAll('.controls__players')
+//playerItems.forEach(item => {
+  // item.addEventListener('click', (e) => {
+    //  console.log(e.path[0].textContent)
+   //})
+//})
 
 // ---------------------
 // Drag and drop player 
 // ---------------------
-
+ // Sets the data to transfer on drag
+ function dragPlayer(ev) {
+   ev.dataTransfer.setData("text", ev.target.textContent)
+}
 
 //overrides default behviour of the dragEvent not dropable
 function allowPlayerDrop(ev) { 
    ev.preventDefault();
  }
- 
- // Sets the data to transfer on drag
-function dragPlayer(ev) {
-   console.log(ev)
-   ev.dataTransfer.setData("text", ev.target.textContent)
-}
 
-// Stuff to do on drop
+
+// Drop the player onto the shirt
 function dropPlayer(ev) {
    ev.preventDefault();
-   let data = ev.dataTransfer.getData("text");
-   console.log(data)
+   let playerData = ev.dataTransfer.getData("text");
+   let playerLabel = ev.path[2].childNodes[3]
+   playerLabel.textContent = playerData
  }
 
+// Listen for click events on the player label
+//let playerLabels = document.querySelectorAll('.pitch__player-label')
+//for (let i = 0; i < playerLabels.length; i++ ) {
+ //  playerLabels[i].addEventListener('click', function(){
+   //   playerLabels[i].textContent = i + "John"
+     // console.log(i)
+  // })
+//}
 
